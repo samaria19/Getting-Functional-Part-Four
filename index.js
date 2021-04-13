60e09d18d6311e32b705cfcffb62f731e618fcc2
Samaria Smothers <>
2021-04-13 17:31:09 +0000

    1/2

--- index.js
+++ index.js
@@ -11,16 +11,22 @@ accept a string and an array as arguements.
 The output should be a number. 
 
 */
-var count = 0
+// var count = 0
 
-function genderCount(array, string){
+// function genderCount(array, string){
+// //CODE BELOW HERE
+// for(var i=0; i <= customers.length - 1; i++ ){
+//   if(customers[i].gender === string ){
+//      count++ 
+//   }
+// }
+
+// console.log(count)
+// //CODE ABOVE HERE
+// }
+
+function gendercount(array)
 //CODE BELOW HERE
-for(var i=0; i <= customers.length - 1; i++ ){
-  if(customers[i].gender === string ){
-     count++ 
-  }
-}
-
-console.log(count)
-//CODE ABOVE HERE
+for(var i=0; i<= customers.length -1; i++){
+  if(customers[i].gender === string)
 }
